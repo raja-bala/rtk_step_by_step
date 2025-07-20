607b7a72319f09d2f70c479de21f6555381042b0
raja-bala <bharathiraja.techie@gmail.com>
2025-07-20 15:59:17 +0800

    Add created redux slice to the store

--- src/store.ts
+++ src/store.ts
@@ -1,3 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
+import jsonPlaceholderApi from "./services/JsonPlaceholderAPI";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
-const store = configureStore({});
+const store = configureStore({
+  reducer: {
+    [jsonPlaceholderApi.reducerPath]: jsonPlaceholderApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(jsonPlaceholderApi.middleware),
+});
+
+setupListeners(store.dispatch);
+
+export default store;
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
