253efc4b624ffda75f8afb2516dedc3d38124db4
raja-bala <bharathiraja.techie@gmail.com>
2025-07-20 13:02:08 +0800

    create a redux slice

---
+++ src/services/JsonPlaceholderAPI.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const jsonPlaceholderApi = createApi({
+  reducerPath: "jsonPlaceholderApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+    }),
+    createPost: builder.mutation({
+      query: (newPost) => ({
+        url: "/posts",
+        method: "POST",
+        body: newPost,
+      }),
+    }),
+  }),
+});
+
+export const { useGetPostsQuery, useCreatePostMutation } = jsonPlaceholderApi;
+
+export default jsonPlaceholderApi;
