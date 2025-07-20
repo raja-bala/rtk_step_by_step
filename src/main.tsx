0de7e068f51ece7f585812dfc17ca980b1801ef3
raja-bala <bharathiraja.techie@gmail.com>
2025-07-20 16:34:19 +0800

    Wrap the App component by AppProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
+ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+// import { ApiProvider } from '@reduxjs/toolkit/query/react'
+// import jsonPlaceholderApi from './services/JsonPlaceholderAPI.ts'
+import { Provider } from 'react-redux'
+import store from './store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    {/** To use only a particular API from redux */}
+    {/* <ApiProvider api={jsonPlaceholderApi}>
+      <App />
+    </ApiProvider> */}
+  }<Provider store={store}> 
     <App />
+  </Provider>
+    
   </StrictMode>,
 )
